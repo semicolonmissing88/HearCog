74b4f638f94f2496abfe9331a9e050fda45a6c31
fahad4787 <nfahad4787@gmail.com>
2026-02-12 19:09:40 +0500

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/HearCog">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
